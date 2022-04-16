ac87e4ae4008d16223ea69094a0e27bee6e62e17
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-04-16 17:18:47 +0530

    Create 1163A-Eating Soup.cpp

---
+++ 1163A-Eating Soup.cpp
@@ -0,0 +1,27 @@
+
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int main(){
+    int n,m; cin>>n>>m;
+    if(m==0) cout<<1;
+    else if(m<=n/2) cout<<m;
+    else cout<<n-m;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
