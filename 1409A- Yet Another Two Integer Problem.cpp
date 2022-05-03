e785a6f6f74616d5855a87c48532b8952b8e8dc5
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-05-03 10:32:21 +0530

    Create 1409A- Yet Another Two Integer Problem.cpp

---
+++ 1409A- Yet Another Two Integer Problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int t; cin>>t;
+    while(t--){
+long long int n,m;
+cin>>n>>m;
+int l=abs(n-m);
+if(l%10!=0) cout<<l/10+1<<endl;
+else cout<<l/10<<endl;
+ 
+ 
+}
+}
