28ed24a2966882d29cd6b7f7d53b661143ac9b9d
Skaezr_73_Blitzkrieg <75065361+Svastik73@users.noreply.github.com>
2022-05-03 10:31:19 +0530

    Create 1663C-Pōja Verdon.cpp

---
+++ 1663C-Pōja Verdon.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main()
+{
+                                //APRIL FOOLS! CONTEST
+  /*_* 
+  //@_@'
+    int x;
+    cin>>x;
+    int a[x];
+    for (int i=0;i<x;i++){
+        cin>>a[i];
+    }
+    int sum=0;
+    for (int i=0;i<x;i++){
+        sum=sum+a[i];
+    }
+    cout<<sum;
+    
+}
